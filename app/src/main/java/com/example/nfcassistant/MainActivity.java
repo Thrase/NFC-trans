71fb07e2cae1bae991b1109c69c10f8282b25575
Thrase <thrase@163.com>
2019-11-21 11:01:14 +0800

    做了个界面，嘿嘿！

--- app/src/main/java/com/example/nfcassistant/MainActivity.java
+++ app/src/main/java/com/example/nfcassistant/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.nfcassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Service;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    String msg = "Android: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(msg, "Oncreate");
     }
+
+    public void startService(View view){
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    /** 当活动即将可见时调用 */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** 当活动可见时调用 */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** 当其他活动获得焦点时调用 */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** 当活动不再可见时调用 */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** 当活动将被销毁时调用 */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+
 }
